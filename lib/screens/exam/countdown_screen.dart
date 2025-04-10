57221c061b37933e541c975ff4d6e4605edaac76
Louis Le Nezet <louislenezet@gamil.com>
2025-04-10 16:28:18 +0200

    Add countdown timer

--- lib/screens/exam/countdown_screen.dart
+++ lib/screens/exam/countdown_screen.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
-import '../../widgets/placeholder_scaffold.dart';
+import '../../widgets/countdown_timer.dart'; // Import the CountdownTimer widget
+import '../../routes.dart';
 
 class CountdownScreen extends StatelessWidget {
   const CountdownScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const PlaceholderScaffold(title: 'Countdown Screen');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Countdown'),
+      ),
+      body: CountdownTimer(
+        duration: 5, // Set the countdown duration here (in seconds)
+        onFinish: () {
+          Navigator.pushReplacementNamed(context, AppRoutes.evaluation); // Navigate to the next screen when finished
+        },
+      ),
+    );
   }
 }
--- lib/screens/exam/exam_menu_screen.dart
+++ lib/screens/exam/exam_menu_screen.dart
@@ -1,11 +1,56 @@
 import 'package:flutter/material.dart';
-import '../../widgets/placeholder_scaffold.dart';
+import '../../constants/colors.dart';
+import '../../routes.dart';
 
 class ExamMenuScreen extends StatelessWidget {
   const ExamMenuScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const PlaceholderScaffold(title: 'Staring exam Screen');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Aikido Exam Helper"),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Welcome Text
+            const Text(
+              "Let's Start Your Exam",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height: 50),
+
+            // Start Button
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to Countdown Screen
+                Navigator.pushNamed(context, AppRoutes.countdown);
+              },
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(double.infinity, 50), // Full width
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                foregroundColor: AppColors.textColor, // Text color
+                backgroundColor: AppColors.buttonColor, // Button color
+              ),
+              child: const Text(
+                "Start Exam",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

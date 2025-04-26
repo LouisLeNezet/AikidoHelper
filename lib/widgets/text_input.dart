bf1c3f300b1ad643cbae7965059611ddb0ede2e6
LouisLeNezet <louislenezet@gmaio.com>
2025-04-26 20:23:01 +0200

    Add text input widget

---
+++ lib/widgets/text_input.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class TextInput extends StatelessWidget {
+  final void Function(String textValue) onChanged;
+  final String hintText;
+  final String title;
+  final String initialValue;
+
+  const TextInput({
+    super.key,
+    required this.onChanged,
+    required this.hintText,
+    required this.title,
+    required this.initialValue,  // Added this parameter to set default value
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 8),
+        TextField(
+          controller: TextEditingController(text: initialValue),  // Set default value
+          onChanged: onChanged,
+          decoration: InputDecoration(
+            hintText: hintText,
+            border: const OutlineInputBorder(),
+          ),
+        ),
+      ],
+    );
+  }
+}
